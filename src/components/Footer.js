290f266e2e81dd261971292d37b1a58de8115697
eS <evansmithcodes@gmail.com>
2019-09-12 22:00:45 -0700

    footer formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,15 @@ import gatsbyLogo from "../images/Gatsby_Logo_Black.svg";
 const Footer = () => {
     return(
         <div className="Footer">
+            <div className="email-call">
+                <p>lorem lorem lorem lorem lorem</p>
+            </div>
             <div className="build-tag">
                 <p>Lovingly Built with</p>
                 <img src={gatsbyLogo} alt="gatsby-logo"></img>
+            </div>
+            <div className="social">
+                <p>lorem lorem lorem lorem lorem</p>
             </div>
         </div>
     )
