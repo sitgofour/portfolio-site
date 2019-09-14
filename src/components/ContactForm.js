58ff571eb67356d2fd7f0fb2f2e0a9144e73e312
eS <evansmithcodes@gmail.com>
2019-09-14 13:31:14 -0700

    contact form formatting and styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import '../styles/ContactForm.css';
 
 const ContactForm = () => {
     return(
         <div className="Contact-form">
             <form>
-                <label>
-                    First Name
-                    <input placeholder="" type="text" name="firstname" id="firstname"/>
-                </label>
-                <label>
-                    Last Name
-                    <input type="text" name="lastname" id="lastname"/>
-                </label>
-                <label>
-                    Email
-                    <input type="email" name="email" id="email"/>
-                </label>
-                <label>
-                    Name
-                    <input type="text" name="name" id="name"/>
-                </label>
+                <div className="firstname">
+                    <label>
+                        First Name
+                        <input placeholder="" type="text" name="firstname" id="firstname"/>
+                    </label>
+                </div>
+                <div className="lastname">
+                    <label>
+                        Last Name
+                        <input placeholder="" type="text" name="lastname" id="lastname"/>
+                    </label>
+                </div>
+                <div className="email">
+                    <label>
+                        Email
+                        <input placeholder="" type="email" name="email" id="email"/>
+                    </label>
+                </div>
+                <div className="message">
+                    <label>
+                        Message
+                        <textarea rows="50" cols="75" name="message" id="message"/>
+                    </label>
+                </div>
             </form>
         </div>
     )
