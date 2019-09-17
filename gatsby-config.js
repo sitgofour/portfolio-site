4f7906c45a6c52a23c9806fb58492c5b9f502315
eS <evansmithcodes@gmail.com>
2019-09-16 21:15:58 -0700

    added typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,12 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      }
+    },
   ]
 
 }
