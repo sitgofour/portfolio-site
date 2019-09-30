a39498c5410b2af3bdf6e9ac5dc0eb2e34b7e05f
eS <evansmithcodes@gmail.com>
2019-09-30 12:42:05 -0700

    out with old contact components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
-import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
     return(
         <div className="Contact-page-wrapper">
             <PageLayout>
-                <ContactForm />
             </PageLayout>
         </div>
     )
