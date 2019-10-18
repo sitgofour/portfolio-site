c586682015ed88bcbaf9dbb4abd337e47dc4adbd
eS <evansmithcodes@gmail.com>
2019-10-18 09:31:54 -0700

    updated footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,8 @@ const Footer = () => {
                 you can also reach me here
             </div>
             <div className="footer-links-container">
-                <a className="footer-link" href="/"><img src={linkedIcon} alt="linked in link"/></a>
-                <a className="footer-link" href="/"><img src={githubIcon} alt="github link"/></a>
+                <a className="footer-link" href="https://www.linkedin.com/in/evan-smith-45600b97/" target="_blank" rel="noopener noreferrer"><img src={linkedIcon} alt="linked in link"/></a>
+                <a className="footer-link" href="https://www.github.com/sitgofour" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="github link"/></a>
                 <a className="footer-link" href="/"><img src={twitterIcon} alt="twitter link"/></a>
             </div>
         </div>
