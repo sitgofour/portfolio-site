6aa69f1806f601c054693bbc074a1863252ae009
sitgofour <evansmithcodes@gmail.com>
2020-06-02 13:18:43 -0700

    basic home styling changes

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/HomeLayout.css';
 
-const HomeLayout = (props) => {
-    return(
-        <div className="page-wrapper">
-            {props.children}
-        </div>
-    )
+class HomeLayout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            style : "page-wrapper-night",
+            styleName: "night"
+        }
+        this.toggleStyle = this.toggleStyle.bind(this);
+    }
+
+    toggleStyle() {
+        console.log("toggle style clicked");
+        if(this.state.style === "page-wrapper-night") {
+            this.setState({
+                style: "page-wrapper-day",
+                styleName: "day"
+            });
+        } else {
+            this.setState({
+                style: "page-wrapper-night",
+                styleName: "night"
+            });
+        }
+    }
+
+    render() {
+        return(
+            <div className={this.state.style}>
+                <div>
+                    <button onClick={this.toggleStyle}>{this.state.styleName}</button>
+                </div>
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
 
