8106d7281879acf02dc278d5ed743a6ffcf04c84
sitgofour <evansmithcodes@gmail.com>
2020-06-20 13:37:54 -0700

    more page meta data

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
 import ContactBody from '../components/ContactBody';
+import { Helmet } from 'react-helmet';
 
 const Contact = () => {
     return(
         <div className="Contact-page-wrapper">
             <PageLayout>
+                <Helmet>
+                    <title>Contact - Evan</title>
+                    <meta name="description" content="Evan Smith's portfolio contact page" />
+                </Helmet>
                 <ContactBody />
             </PageLayout>
         </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
 import ProjectsBody from '../components/ProjectsBody';
+import { Helmet } from 'react-helmet';
 
 const Projects = () => {
     return(
         <div className="Projects-page-wrapper">
+            <Helmet>
+                <title>Projects - Evan</title>
+                <meta name="description" content="Evan Smith's portfolio projects page" />
+            </Helmet>
             <PageLayout>
                 <ProjectsBody />
             </PageLayout>
